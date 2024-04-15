613c1ec2309e040254dc5fb539f227ddda4fdc45
Nandhakumar S <126797135+Nandhakumar868@users.noreply.github.com>
2024-04-15 13:22:28 +0530

    Access Token added (#8)

--- SmartAgile_Backend/Users/urls.py
+++ SmartAgile_Backend/Users/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import UsersView , UserEditUpdateDeleteView,LoginView
+from .views import UsersView , UserEditUpdateDeleteView, LoginView
+
+# from .views import MyTokenObtainPairView
+
+# from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('employees/', UsersView.as_view(), name='Users'),
     path('employees/<int:id>' ,UserEditUpdateDeleteView.as_view() , name="User Edits" ),
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    # path('token/', MyTokenObtainPairView.as_view(), name='token-obtain-pair'),
+    # path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh-view'),
 ]
--- SmartAgile_Backend/Users/views.py
+++ SmartAgile_Backend/Users/views.py
@@ -1,10 +1,27 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import Token
 from .serializers import UserSerializer
 from rest_framework import status
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from rest_framework.generics import ListCreateAPIView , RetrieveUpdateDestroyAPIView
+
+# from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+# from rest_framework_simplejwt.views import TokenObtainPairView
+# from rest_framework.authtoken.models import Token
+# from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
+
+# class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+#     @classmethod
+#     def get_token(cls, user):
+#         token =  super().get_token(user)
+#         token['username'] = user.username
+#         token['email'] = user.email
+#         return token
+    
+# class MyTokenObtainPairView(TokenObtainPairView):
+#     serializer_class = MyTokenObtainPairSerializer
 
 class UsersView(ListCreateAPIView):
     queryset = User.objects.all()
@@ -15,36 +32,22 @@ class UserEditUpdateDeleteView(RetrieveUpdateDestroyAPIView):
     serializer_class = UserSerializer
     lookup_field = 'id'
 
-# class RegisterView(APIView):
-
-#     def get(self,request):
-#         username = User.objects.all()
-#         serializer = UserSerializer(username,many=True)
-#         return Response(serializer.data)
-
-#     def post(self,request):     
-        
-#         serializer = UserSerializer(data=request.data)
-
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response({'message' : 'User registration successful'}, status=status.HTTP_201_CREATED)
-#         else:
-#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
 class LoginView(APIView):
     def post(self,request):
-        username = request.data.get('username')
+        email = request.data.get('email')
         password = request.data.get('password')
 
-        user = authenticate(username=username,password=password)
+        user = authenticate(email=email,password=password)
 
         if not user:
             return Response({'error' : 'Invalid Credentials'},status=status.HTTP_400_BAD_REQUEST)
         else:
-
+            # access_token = AccessToken.for_user(user)
+            # refresh_token = RefreshToken.for_user(user)
             return Response({
-                    'username' : username,
+                    # 'access_token' : str(access_token),
+                    # 'refresh_token' : str(refresh_token),
+                    'email' : email,
                     'message' : 'Logged in successfully',
                     'is_staff' : user.is_staff
                     },status=status.HTTP_200_OK)
