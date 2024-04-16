f12f4e6df90dd61de30ebed16d6618059ecc7e0e
tmachine <tmachine@SYS22.PEC.IN>
2024-04-16 13:03:37 +0530

    superuser-serializers and url's

--- SmartAgile_Backend/Users/urls.py
+++ SmartAgile_Backend/Users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UsersView , UserEditUpdateDeleteView, LoginView
+from .views import UsersView , UserEditUpdateDeleteView, LoginView,SuperuserView
 
 # from .views import MyTokenObtainPairView
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('employees/', UsersView.as_view(), name='Users'),
     path('employees/<int:id>' ,UserEditUpdateDeleteView.as_view() , name="User Edits" ),
     path('login/', LoginView.as_view(), name='login'),
+    path('superuser/',SuperuserView.as_view(),name='superuser-create')
     # path('token/', MyTokenObtainPairView.as_view(), name='token-obtain-pair'),
     # path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh-view'),
 ]
