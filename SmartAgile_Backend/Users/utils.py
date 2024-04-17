8095e2f8f864f99307f996cf59bddf766020f536
Sakthikarthick <117448824+Sakthikarthick3107@users.noreply.github.com>
2024-04-17 11:33:21 +0530

    Nandhakumar -> forget password set
    * Forget Password with Nandhakumar
    
    * Forgot Password Modified
    
    * Forgot Password Modified
    
    ---------
    
    Co-authored-by: Nandhakumar <kumarnandha8983@gmail.com>

---
+++ SmartAgile_Backend/Users/utils.py
@@ -0,0 +1,29 @@
+from django.core.mail import send_mail
+import secrets
+from django.conf import settings
+
+base_url = 'http://127.0.0.1:8000/users'
+
+def generate_password_reset_otp():
+    return ''.join([str(secrets.choice('0123456789')) for i in range(6)])
+
+def send_password_reset_email(user, uid, token):
+
+    # unique_token = os.urandom(32).hex()
+
+    otp = generate_password_reset_otp()
+    user.otp = otp
+
+    reset_link = f"{base_url}/auth/password_reset/confirm/otp/{uid}/{token}/"
+
+    email_body = f"""One Time Password: {otp}
+                 Click the link below to reset your password:\n{reset_link}"""
+
+    send_mail(
+        subject="Password Reset Request",
+        message=email_body,
+        recipient_list=[user.email],
+        from_email=settings.EMAIL_HOST_USER
+    )
+
+    user.save()
