9393266246719ba5be948d05802b3373512a3f9f
Sakthikarthick <117448824+Sakthikarthick3107@users.noreply.github.com>
2024-04-30 13:37:46 +0530

    Update urls.py

--- SmartAgile_Backend/Projects/urls.py
+++ SmartAgile_Backend/Projects/urls.py
@@ -3,5 +3,5 @@ from .views import ProjectView
 
 urlpatterns = [
     path('', ProjectView.as_view(), name='Project Created'),
-    path('projects/<int:id>', ProjectView.as_view(), name='Projects Details')
+    path('<int:id>', ProjectView.as_view(), name='Projects Details')
 ]
