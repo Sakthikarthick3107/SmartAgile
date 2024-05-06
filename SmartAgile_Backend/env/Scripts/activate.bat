a06a304e066ddaec48a8b5b440b9f75ca1c0b22d
Sakthikarthick <sakthikarthick3107@gmail.com>
2024-05-06 11:38:03 +0530

    env reset

--- SmartAgile_Backend/env/Scripts/activate.bat
+++ SmartAgile_Backend/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\T-Machine\SmartAgile\SmartAgile_Backend\env
+set VIRTUAL_ENV=D:\T Machine\AI-AWAM\T-Machine-SmartAgile\SmartAgile_Backend\env
 
 if not defined PROMPT set PROMPT=$P$G
 
--- SmartAgile_Mobile/env.js
+++ SmartAgile_Mobile/env.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'https://c244-14-195-39-82.ngrok-free.app';
+export const baseUrl = 'https://3394-52-66-90-13.ngrok-free.app';
