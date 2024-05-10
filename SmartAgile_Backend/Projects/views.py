7d1ccd516fa53a4953801e840e10d60b9d20b15c
Nandhakumar <kumarnandha8983@gmail.com>
2024-05-10 08:07:48 +0530

    Projects error resolved

--- SmartAgile_Backend/Projects/views.py
+++ SmartAgile_Backend/Projects/views.py
@@ -54,10 +54,10 @@ class ProjectView(APIView):
             return Response({'message' : 'Project Created Successfully'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
-    def put(self,request,id):
+    def put(self,request,proj_id):
         try:
-            project = Project.objects.get(id=id)
-        except project.DoesNotExist:
+            project = Project.objects.get(pk=proj_id)
+        except Project.DoesNotExist:
             return Response({'message' : 'Project Not found'}, status=status.HTTP_400_BAD_REQUEST)
         
         serializer = ProjectSerializer(project, data = request.data)
@@ -66,11 +66,11 @@ class ProjectView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def delete(self,request,id):
-        project = Project.objects.get(id=id)
+    def delete(self,request,proj_id):
         try:
+            project = Project.objects.get(pk=proj_id)
             project.delete()
             return Response({'message' : 'Project Deleted Successfully'},status=status.HTTP_200_OK)
-        except project.DoesNotExist:
+        except Project.DoesNotExist:
             return Response({'message' : 'Project Not found'}, status=status.HTTP_400_BAD_REQUEST)
 
