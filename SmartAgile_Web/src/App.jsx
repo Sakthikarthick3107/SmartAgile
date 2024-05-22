92c75ffc2a72a36ad9f8c6ffae6f4c2525cda4f5
tmachine <tmachine@sys04.PEC.IN>
2024-05-22 12:04:01 +0530

    employee and supervisor navigation in login

--- SmartAgile_Web/src/App.jsx
+++ SmartAgile_Web/src/App.jsx
@@ -1,52 +1,3 @@
-// import React from "react";
-// import { Routes, Route,Outlet } from "react-router-dom";
-// import Login from "./pages/Login";
-// import Organization from "./pages/Organization";
-// import FirstPage from "./pages/FirstPage";
-// import Navbar from "./components/EmployeeComponents/NavBar";
-// import Sidebar from "./components/EmployeeComponents/Sidebar";
-// // index.js or App.js
-// import '@fortawesome/fontawesome-free/css/all.min.css';
-// // import { Chat, Dashboard, Settings } from "@mui/icons-material";
-// import Projects from "./pages/EmployeePages/Projects";
-// import TaskHub from "./pages/EmployeePages/Taskhub";
-// import Chat from "./pages/EmployeePages/Chat";
-// import Settings from "./pages/EmployeePages/Settings";
-// import Dashboard from "./pages/EmployeePages/Dashboard";
-
-// function App() {
-//   function DashboardLayout() {
-//     return (
-//       <div className="flex flex-col h-screen">
-//         <Navbar />
-//         <div className="flex flex-grow">
-//         <Sidebar />
-//         <Outlet />
-//       </div>
-//       </div>
-//     );
-//   }
-//   return (
-//       <Routes>
-//         <Route path="/" element={<FirstPage />} />
-//         <Route path="/Login" element={<Login />} />
-//         <Route path="/Organization" element={<Organization />} />
-//         <Route path="/Login/*" element={<DashboardLayout />} >
-//         <Route path="dashboard" element={<Dashboard/>} />
-//             <Route path="projects" element={<Projects />} />
-//             <Route path="tasks" element={<TaskHub />} />
-//             <Route path="chat" element={<Chat />} />
-//             <Route path="settings" element={<Settings/>} />
-//         </Route>
-//       </Routes>
-    
-//   );
-// }
-
-// export default App;
-
-
-
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
@@ -61,6 +12,7 @@ import Chat from "./pages/EmployeePages/Chat";
 import Settings from "./pages/EmployeePages/Settings";
 import Dashboard from "./pages/EmployeePages/Dashboard";
 import TeamDetails from "./pages/EmployeePages/TeamDetails";
+import SDashboard from "./pages/SupervisorPages/SDashboard";
 
 // Layout component with Navbar and Sidebar
 function LayoutWithSidebar({ children }) {
@@ -87,6 +39,10 @@ function App() {
       <Route path="/chat" element={<LayoutWithSidebar><Chat /></LayoutWithSidebar>} />
       <Route path="/chat/:teamId" element={<LayoutWithSidebar><TeamDetails /></LayoutWithSidebar>} />
       <Route path="/settings" element={<LayoutWithSidebar><Settings /></LayoutWithSidebar>} />
+
+
+
+      <Route path="/sdashboard" element={<SDashboard />} />
     </Routes>
   );
 }
