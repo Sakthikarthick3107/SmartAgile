62f84fba5ef31dd7a643e4b2e8f2a1438227b82d
Nandhakumar <kumarnandha8983@gmail.com>
2024-05-24 09:24:33 +0530

    Added status in tasks

--- SmartAgile_Backend/Tasks/models.py
+++ SmartAgile_Backend/Tasks/models.py
@@ -8,6 +8,11 @@ class Task(models.Model):
         ('MED', 'Medium'),
         ('HIGH', 'High'),
     ]
+    STATUS_CHOICES = [
+        ('TD', 'To-do'),
+        ('IP', 'Inprogress'),
+        ('CP', 'Completed')
+    ]
     project = models.ForeignKey(Project, related_name='project_tasks', on_delete=models.CASCADE)
     assigned_to = models.ForeignKey(ProjectMembers,related_name='project_member', on_delete=models.CASCADE,blank=True,null=True)
     task_id = models.AutoField(primary_key=True)
@@ -15,6 +20,7 @@ class Task(models.Model):
     task_deadline = models.DateField()
     task_priority = models.CharField(max_length=15, choices=PRIORITY_CHOICES, default='LOW')
     task_desc = models.CharField(max_length=255)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='TD')
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
