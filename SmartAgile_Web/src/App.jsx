ecc930660e7051e50db2079e19cfc202380ab9a3
tmachine <tmachine@sys04.PEC.IN>
2024-05-27 12:48:19 +0530

    chat and supervisor employees page update

--- SmartAgile_Web/src/App.jsx
+++ SmartAgile_Web/src/App.jsx
@@ -40,8 +40,6 @@
 //       <Route path="/chat/:teamId" element={<LayoutWithSidebar><TeamDetails /></LayoutWithSidebar>} />
 //       <Route path="/settings" element={<LayoutWithSidebar><Settings /></LayoutWithSidebar>} />
 
-
-
 //       <Route path="/sdashboard" element={<SDashboard />} />
 //     </Routes>
 //   );
@@ -49,16 +47,14 @@
 
 // export default App;
 
-
-
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Organization from "./pages/Organization";
 import FirstPage from "./pages/FirstPage";
 import Navbar from "./components/EmployeeComponents/NavBar";
 import Sidebar from "./components/EmployeeComponents/Sidebar";
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import Projects from "./pages/EmployeePages/Projects";
 import TaskHub from "./pages/EmployeePages/Taskhub";
 import Chat from "./pages/EmployeePages/Chat";
@@ -71,7 +67,8 @@ import Side from "./components/Supervisor/Side";
 import SProjects from "./pages/SupervisorPages/SProjects";
 import SEmployees from "./pages/SupervisorPages/SEmployees";
 import SSettings from "./pages/SupervisorPages/SSettings";
-
+import AddCandidate from "./components/Supervisor/ProjectComponents/AddCandidate";
+import AddProject from "./components/Supervisor/ProjectComponents/AddProject";
 
 // Layout component with Navbar and Sidebar
 function LayoutWithSidebar({ children }) {
@@ -98,48 +95,129 @@ function LayoutWithTop({ children }) {
   );
 }
 
+function App() {
+  const [isStaff, setIsStaff] = useState(null);
 
-  function App() {
-    const [isStaff, setIsStaff] = useState(null);
-  
-    useEffect(() => {
-      const user = JSON.parse(localStorage.getItem("user"));
-      if (user) {
-        setIsStaff(user.isStaff);
-      }
-    }, []);
-  
-    // if (isStaff === null) {
-    //   return <div>Loading...</div>; // Or a spinner/loading component
-    // }
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      setIsStaff(user.isStaff);
+    }
+  }, []);
+
+  // if (isStaff === null) {
+  //   return <div>Loading...</div>; // Or a spinner/loading component
+  // }
   return (
     <Routes>
       <Route path="/" element={<FirstPage />} />
       <Route path="/organization" element={<Organization />} />
       <Route path="/login" element={<Login />} />
-      
+
       {isStaff ? (
-       
         <>
-          <Route path="/sdashboard" element={<LayoutWithTop><SDashboard /></LayoutWithTop>} />
-          <Route path="/sprojects" element={<LayoutWithTop><SProjects /></LayoutWithTop>} />
-          <Route path="/employees" element={<LayoutWithTop><SEmployees /></LayoutWithTop>} />
-          <Route path="/ssettings" element={<LayoutWithTop><SSettings /></LayoutWithTop>} />
-          
+          <Route
+            path="/sdashboard"
+            element={
+              <LayoutWithTop>
+                <SDashboard />
+              </LayoutWithTop>
+            }
+          />
+          <Route
+            path="/sprojects"
+            element={
+              <LayoutWithTop>
+                <SProjects />
+              </LayoutWithTop>
+            }
+          />
+          <Route
+            path="/employees"
+            element={
+              <LayoutWithTop>
+                <SEmployees />
+              </LayoutWithTop>
+            }
+          />
+          <Route
+            path="/ssettings"
+            element={
+              <LayoutWithTop>
+                <SSettings />
+              </LayoutWithTop>
+            }
+          />
         </>
       ) : (
         // Non-staff routes
         <>
-          <Route path="/dashboard" element={<LayoutWithSidebar><Dashboard /></LayoutWithSidebar>} />
-          <Route path="/projects" element={<LayoutWithSidebar><Projects /></LayoutWithSidebar>} />
-          <Route path="/tasks" element={<LayoutWithSidebar><TaskHub /></LayoutWithSidebar>} />
-          <Route path="/chat" element={<LayoutWithSidebar><Chat /></LayoutWithSidebar>} />
-          <Route path="/chat/:teamId" element={<LayoutWithSidebar><TeamDetails /></LayoutWithSidebar>} />
-          <Route path="/settings" element={<LayoutWithSidebar><Settings /></LayoutWithSidebar>} />
+          <Route
+            path="/dashboard"
+            element={
+              <LayoutWithSidebar>
+                <Dashboard />
+              </LayoutWithSidebar>
+            }
+          />
+          <Route
+            path="/projects"
+            element={
+              <LayoutWithSidebar>
+                <Projects />
+              </LayoutWithSidebar>
+            }
+          />
+          <Route
+            path="/tasks"
+            element={
+              <LayoutWithSidebar>
+                <TaskHub />
+              </LayoutWithSidebar>
+            }
+          />
+          <Route
+            path="/chat"
+            element={
+              <LayoutWithSidebar>
+                <Chat />
+              </LayoutWithSidebar>
+            }
+          />
+          <Route
+            path="/chat/:teamId"
+            element={
+              <LayoutWithSidebar>
+                <TeamDetails />
+              </LayoutWithSidebar>
+            }
+          />
+          <Route
+            path="/settings"
+            element={
+              <LayoutWithSidebar>
+                <Settings />
+              </LayoutWithSidebar>
+            }
+          />
+          <Route
+            path="/saddcandidate"
+            element={
+              <LayoutWithTop>
+                <AddCandidate />
+              </LayoutWithTop>
+            }
+          />
+          <Route
+            path="/saddproject"
+            element={
+              <LayoutWithTop>
+                <AddProject />
+              </LayoutWithTop>
+            }
+          />
         </>
       )}
-
-      
     </Routes>
   );
 }
