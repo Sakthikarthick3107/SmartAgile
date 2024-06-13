0a33eae1deab356429a535df34349da05ed1f1ce
Nandhakumar S <126797135+Nandhakumar868@users.noreply.github.com>
2024-06-13 14:32:11 +0530

    Task filtered by Project Member id

--- SmartAgile_Backend/Tasks/urls.py
+++ SmartAgile_Backend/Tasks/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('<int:task_id>/', TaskView.as_view(), name='Task Details'),
     path('project/<int:proj_id>/',Project_Task.as_view(),name='Project id'),
     path('project/<int:proj_id>/prior=<str:task_priority>/' , Project_Task.as_view() , name="Tasks on Priority"),
-    path('project/user-tasks/<int:proj_id>/<int:user_id>/', UserTaskListView.as_view(), name='User Task List'),
+    path('project/user-tasks/<int:proj_id>/<int:id>/', UserTaskListView.as_view(), name='User Task List'),
 ]
