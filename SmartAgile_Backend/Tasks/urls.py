920d5e118212c76a8819027a845c0056aeeac7aa
Nandhakumar <kumarnandha8983@gmail.com>
2024-06-18 14:22:20 +0530

    Filter tasks by user id only

--- SmartAgile_Backend/Tasks/urls.py
+++ SmartAgile_Backend/Tasks/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskView,Project_Task, UserTaskListView, UserTaskView
+from .views import TaskView,Project_Task, UserTaskListView, UserTaskView, UserTaskLists
 
 urlpatterns = [
     path('', TaskView.as_view(), name='Task Created'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('project/<int:proj_id>/prior=<str:task_priority>/' , Project_Task.as_view() , name="Tasks on Priority"),
     path('project/user-tasks/<int:proj_id>/<int:id>/', UserTaskListView.as_view(), name='User Task List'),
     path('projects/user-task-data/<int:proj_id>/<int:user_id>/', UserTaskView.as_view(), name='User-Task_View'),
+    path('projects/user-task/<int:user_id>/', UserTaskLists.as_view(), name='User-Task_View'),
 ]
