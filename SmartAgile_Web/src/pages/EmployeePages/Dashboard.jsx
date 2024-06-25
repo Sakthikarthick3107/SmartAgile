d7fb0aca2e5262770ad920dcb85cbd5be0121a43
ManoSree <127037647+ManoSree@users.noreply.github.com>
2024-06-25 13:39:03 +0530

    Update Dashboard

--- SmartAgile_Web/src/pages/EmployeePages/Dashboard.jsx
+++ SmartAgile_Web/src/pages/EmployeePages/Dashboard.jsx
@@ -1,18 +1,24 @@
 
-import React from "react";
-// import BarChart from "../../components/SupervisorComponents/BarChart";
-import ProjectBarchart from "../../components/EmployeeComponents/DashboardComponents/ProjectBarchart";
-import Doughnet from "../../components/EmployeeComponents/DashboardComponents/Doughnet";
 
+import React from "react";
+import ProjectBarchart from "../../components/EmployeeComponents/DashboardComponents/ProjectBarchart";
+import Doughnut from "../../components/EmployeeComponents/DashboardComponents/Doughnet";
+import Calender from "../../components/SupervisorComponents/DashboardComponents/Calender";
 
 const Dashboard = () => {
   return (
-    <div >
+    <div className="relative">
       Dashboard
-    
-      <ProjectBarchart/>
-      <Doughnet/>
 
+      <ProjectBarchart />
+      <Doughnut />
+
+      {/* Positioning Calendar in the top right corner */}
+      <div className="absolute top-2 right-2">
+        <div className="rounded-lg p-4">
+          <Calender />
+        </div>
+      </div>
     </div>
   );
 };
